bb86a0bea44f39f69438ba121673590eb2c7adea
arikBizwatch <arikmozh@gmail.com>
2023-11-26 18:31:32 +0200

    added atlas db

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = () => {
+  // mongoose
+  //   .connect("mongodb://localhost:27017/WorkersManagement")
+  //   .then(() => console.log("Connected to WorkersManagementDB!"))
+  //   .catch((error) => console.log(error));
   mongoose
-    .connect("mongodb://localhost:27017/WorkersManagement")
+    .connect(
+      "mongodb+srv://arikbars:arikbars123@cluster.yfn6rie.mongodb.net/?retryWrites=true&w=majority"
+    )
     .then(() => console.log("Connected to WorkersManagementDB!"))
     .catch((error) => console.log(error));
 };
